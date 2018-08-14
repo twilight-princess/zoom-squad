4d994e8c300e2f404151e4556b940743fc24cba2
darlene <evaelidar@gmail.com>
2018-08-13 18:16:21 -0600

    redux started

---
+++ routes/animals.js
@@ -0,0 +1,40 @@
+const express = require('express')
+
+const animalRouter = express.Router()
+const Animal = require('../models/animal.js')
+
+animalRouter.route('/')
+    .get((req, res) => {
+        Animal.find().exec((err, animal) => {
+            res.send(animal)
+        })
+    })
+    .post((req,res) => {
+        console.log(req.body)
+        let animal = new Animal(req.body)
+        animal.save((err) => {
+            if (err) res.send(err)
+            res.send({status: true})
+        })
+    })
+animalRouter.route('/:id')
+    .get((req, res) => {
+        Animal.findOne({_id: req.params.id}, (err, animal) => {
+            if (err) res.send(err)
+            res.send(animal)
+        })
+    })
+    .put((req, res) => {
+        Animal.findOneAndUpdate({_id: req.params.id}, req.body, (err, animal) => {
+            if (err) res.send(err)
+            res.send({status: true})
+        })
+    })
+    .delete((req, res) => {
+        Animal.findOneAndRemove({_id: req.params.id}, (err) => {
+            if (err) res.send(err)
+            res.send({status: true})
+        })
+    })
+
+module.exports = animalRouter
