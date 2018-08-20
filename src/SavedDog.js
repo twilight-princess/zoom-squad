7740fb4a4ce66d6dc20f663edb319324e27de2c5
Tink <evaelidar@gmail.com>
2018-08-20 10:36:19 -0600

    stuff for single woof

--- src/SavedDog.js
+++ src/SavedDog.js
@@ -2,6 +2,7 @@
 import Navbar from './Navbar';  
 import React, { Component } from 'react'
 import axios from 'axios'
+
 export default class SavedDog extends Component {
     constructor(props){
         super(props)
@@ -9,28 +10,27 @@ export default class SavedDog extends Component {
             currdog:{},
         }
     }
-    componentWillMount(){
+
+    componentDidMount(){
         axios.get(`https://vschool-cors.herokuapp.com?url=http://api.petfinder.com/pet.get?key=b2d01f997b5d0d9a277ff943820a934c&id=${this.props.match.params.id}&format=json`)
-            .then(response =>{
-                this.setState({currdog: response.data.petfinder.pet})
-            }).catch(function (err) {
-                console.log(err)
-            })
-        }
-        componentDidMount(){
-                console.log(this.state.currdog.name)
-        }
-        render(){
+        .then(response =>{
+            this.setState({currdog: response.data.petfinder.pet})
+        }).catch(function (err) {
+            console.log(err)
+        })
+    }
+    render(){
         let doggo= () => {
             return(
                 <div>
                     Hello
-                    </div>
+                </div>
             )
         }
-    return (
-        <div>
-            {console.log(this.state.currdog.name)}
-            {doggo}
-        </div>
-    )}}
+        return(
+            <div>
+                {console.log(this.state.currdog.name)}
+            </div>
+        )
+    }
+}
