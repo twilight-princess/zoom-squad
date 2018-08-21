8f67bf84f8b853d0088d287495f6b9af0a59320f
darlene <evaelidar@gmail.com>
2018-08-20 23:06:23 -0600

    server running

--- server.js
+++ server.js
@@ -15,22 +15,18 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-
-
-app.use(express.static(path.join(__dirname, "public", "build")))
-app.use(cors())
-app.use(bodyParser.json())
-// Routes
-app.use('/api/user', userRouter)
-app.use('/api/animals', animalRouter)
-
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
     .then(db => console.log(`connected to mongodb ${MONGODB_URI}`))
     .catch(err => console.log(err))
 
-app.get("*", (req, res) => {
-    console.log("application served")
-    res.sendFile(path.join(__dirname, "public", "build", "index.html"))
-})
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app
+    .use(express.static(path.join(__dirname, "public")))
+    .use(cors())
+    .use(bodyParser.json())
+    .use('/api/user', userRouter)
+    .use('/api/animals', animalRouter)
+    .get("*", (req, res) => {
+        console.log("application served")
+        res.sendFile(path.join(__dirname, "public", "build", "index.html"))
+    })
+    .listen(PORT, () => console.log(`Server is running on port ${PORT}`))
