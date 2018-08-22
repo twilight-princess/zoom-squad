b267c7e04f0bc18075b83ec15b7cae068d95e7f2
DeclanKottmeier <39426618+DeclanKottmeier@users.noreply.github.com>
2018-08-22 01:51:54 -0400

    Updated Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,17 @@ import Navbar from './Navbar';
 function Contact () {  
     return (
         <div>
+            <div class="frontImage">
+            <img class="frontPhoto" src="http://evelynswinebar.com/wp-content/uploads/2018/06/dogpic.jpg" alt="Picture of Dog"/>
+            </div>
+            <div class="conceptIntro">Contact</div>
+            <div class="emptySpace">
+            <div class="plots1"><h1 class="plotsHead">For more information on Dog Finder contact the developers below</h1>
+                <h5 class="plotText">Darlene Welch - Email: evaelidar@gmail.com | Github: twilight-princess </h5>
+                <h5 class="plotText">Noal Tessman - Email: noal900@gmail.com | Github: NoalTessman </h5>
+                <h5 class="plotText">Declan Kottmeier - Email: declankottmeier@gmail.com | Github: DeclanKottmeier </h5>
+            </div>
+            </div>
         </div>
     )
 }
