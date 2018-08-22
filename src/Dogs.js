b5be0b6c36c943f085e6f2e6fe8719757e5b289e
DeclanKottmeier <39426618+DeclanKottmeier@users.noreply.github.com>
2018-08-22 01:51:18 -0400

    Removed initial dog box
    Dog box that was used as a test and not part of the api has been removed

--- src/Dogs.js
+++ src/Dogs.js
@@ -33,7 +33,6 @@ export default class Dogs extends Component {
             <div className="results">(number) (breed) found between the ages of (min age) and (max age)</div>
             {console.log(this.state.Dogs)}
             <div className="dogGrid">
-                <div className="box1"><div className="text1">Penny | 3 years<Link to="{/Dogs/${Dog.id.$t}}"><img className="dog1" src="https://www.what-dog.net/Images/faces2/scroll001.jpg"/></Link></div></div>
                 {dogList}
             </div>
         </div>
